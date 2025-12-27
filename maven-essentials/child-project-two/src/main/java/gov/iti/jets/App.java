97e436d64f256c4e230716e3e428b29a9de55dfb
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2025-12-27 23:17:58 +0200

    Maven Essentials Labs - Practiced Maven multi-module project structure using a parent POM - Applied dependencyManagement with a custom BOM (my-bom) - Centralized shared dependencies using a dedicated my-dependencies module - Implemented child modules with inherited configurations and dependencies - Built and ran a basic Java application using Maven lifecycle phases - Integrated JavaFX with Maven and verified runtime configuration - Used Maven Wrapper for consistent builds and execution

---
+++ maven-essentials/child-project-two/src/main/java/gov/iti/jets/App.java
@@ -0,0 +1,14 @@
+package gov.iti.jets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
